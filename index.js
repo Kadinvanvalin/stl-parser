9fefab812adeef863cb600bc1f05253526f45814
Kadin Van Valin <kadinvanvalin@gmail.com>
2020-01-23 13:51:23 -0600

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const geo = require("./lib/Geo.js/index.js");
+const stl = require("./lib/stlParser");
+const IO = require("./lib/IO");
+
+function runScript(stlFile) {
+  const listOfTrianglePoints = stl.listOfTrianglePoints(stlFile);
+
+  const lengthOfTriangleSides = listOfTrianglePoints.map(coord =>
+    geo.trianglePointsToLengths(coord)
+  );
+
+  const boundingBox = geo.findBoundingBox(listOfTrianglePoints);
+
+  const SurfaceAreaOfModel = lengthOfTriangleSides
+    .map(coords => geo.sizeOfTriangleHeron(coords.a, coords.b, coords.c))
+    .reduce((a, c) => a + c);
+
+  console.log(
+    ` Surface Area: ${SurfaceAreaOfModel}\n`,
+    `Number of Triangles: ${stl.numberOfTriangles(stlFile)}\n`,
+    "Bounding Box:",
+    IO.boundingBoxRender(boundingBox)
+  );
+}
+
+IO.askQuestion("What is the relative path to the file? ")
+  .then(IO.read)
+  .then(runScript);
---
+++ samples/samples.md
@@ -0,0 +1,7 @@
+https://people.sc.fsu.edu/~jburkardt/data/stla/stla.html
+Sample Files:
+BLOCK is a cube of side 100, using 12 triangular faces.
+
+BOTTLE is a bottle using 1240 triangular faces.
+CUBE is a cube using 12 triangular faces.
+FLAT is a flat shape using 3 triangular faces.
